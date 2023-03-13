6687fe4f6ddb6864da73276df287978f76a8f3ad
DevMentat <devmentat@proton.me>
2023-03-13 16:00:34 +0100

    Add Output folder feature

--- main.go
+++ main.go
@@ -14,16 +14,17 @@ func main() {
 	currentDirectory, _ := os.Getwd()
 
 	var command string
+	outputDir := "output"
 
 	switch flavor {
 	case "latte":
-		command = fmt.Sprintf("cd %s ; magick %s flavors/latte-hald-clut.png -hald-clut %s-%s", currentDirectory, image, flavor, image)
+		command = fmt.Sprintf("cd %s ; magick %s flavors/latte-hald-clut.png -hald-clut %s/%s-%s", currentDirectory, image, outputDir, flavor, image)
 	case "frappe":
-		command = fmt.Sprintf("cd %s ; magick %s flavors/frappe-hald-clut.png -hald-clut %s-%s", currentDirectory, image, flavor, image)
+		command = fmt.Sprintf("cd %s ; magick %s flavors/frappe-hald-clut.png -hald-clut %s/%s-%s", currentDirectory, image, outputDir, flavor, image)
 	case "macchiato":
-		command = fmt.Sprintf("cd %s ; magick %s flavors/macchiato-hald-clut.png -hald-clut %s-%s", currentDirectory, image, flavor, image)
+		command = fmt.Sprintf("cd %s ; magick %s flavors/macchiato-hald-clut.png -hald-clut %s/%s-%s", currentDirectory, image, outputDir, flavor, image)
 	case "mocha":
-		command = fmt.Sprintf("cd %s ; magick %s flavors/mocha-hald-clut.png -hald-clut %s-%s", currentDirectory, image, flavor, image)
+		command = fmt.Sprintf("cd %s ; magick %s flavors/mocha-hald-clut.png -hald-clut %s/%s-%s", currentDirectory, image, outputDir, flavor, image)
 	default:
 		command = ""
 	}
@@ -33,11 +34,16 @@ func main() {
 		return
 	}
 
+	if err := os.MkdirAll(outputDir, 0755); err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
 	cmd := exec.Command("bash", "-c", command)
 	if err := cmd.Run(); err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
 
-	fmt.Printf("Successfully generated image with %s flavor\n", flavor)
+	fmt.Printf("Successfully generated image with %s flavor in %s/%s-%s\n", flavor, outputDir, flavor, image)
 }
