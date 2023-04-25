48dab35214a8dd53c63f24f90fa6f1543ae866cd
DevMentat <devmentat@proton.me>
2023-04-25 10:28:55 +0200

    🔍 refactor(main.go): use map to lookup flavor image path 🐛 fix(main.go): fix output path to include flavor and image name 🎨 style(main.go): remove unnecessary comments and whitespace

--- main.go
+++ main.go
@@ -4,66 +4,51 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 func main() {
-	// Gattopuccino by Devmentat
-	// Get the command line arguments
 	if len(os.Args) != 3 {
 		fmt.Println("Usage: gattopuccino <flavor> <image>")
 		return
 	}
-	flavor := os.Args[1] // The flavor of the coffee
-	image := os.Args[2]  // The name of the input image
+	flavor := os.Args[1]
+	image := os.Args[2]
 
-	// Get the current working directory
 	currentDirectory, err := os.Getwd()
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
 
-	// Set the output directory name
 	outputDir := "output"
-
-	// Create the output directory if it does not exist
 	if err := os.MkdirAll(outputDir, 0755); err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
 
-	// Check if the flavor is recognized
-	var command string
-	switch flavor {
-	case "latte":
-		// Use the "latte" flavor
-		command = fmt.Sprintf("cd %s ; magick %s flavors/latte-hald-clut.png -hald-clut %s/%s-%s", currentDirectory, image, outputDir, flavor, image)
-	case "frappe":
-		// Use the "frappe" flavor
-		command = fmt.Sprintf("cd %s ; magick %s flavors/frappe-hald-clut.png -hald-clut %s/%s-%s", currentDirectory, image, outputDir, flavor, image)
-	case "macchiato":
-		// Use the "macchiato" flavor
-		command = fmt.Sprintf("cd %s ; magick %s flavors/macchiato-hald-clut.png -hald-clut %s/%s-%s", currentDirectory, image, outputDir, flavor, image)
-	case "mocha":
-		// Use the "mocha" flavor
-		command = fmt.Sprintf("cd %s ; magick %s flavors/mocha-hald-clut.png -hald-clut %s/%s-%s", currentDirectory, image, outputDir, flavor, image)
-	default:
-		// Flavor not recognized
+	flavors := map[string]string{
+		"latte":     "flavors/latte-hald-clut.png",
+		"frappe":    "flavors/frappe-hald-clut.png",
+		"macchiato": "flavors/macchiato-hald-clut.png",
+		"mocha":     "flavors/mocha-hald-clut.png",
+	}
+
+	lookupFlavor, ok := flavors[flavor]
+	if !ok {
 		fmt.Println("Error: flavor not recognized")
 		fmt.Println("Usage: gattopuccino <flavor> <image>")
 		return
 	}
 
-	// Build the command based on the chosen flavor
-	cmd := exec.Command("bash", "-c", command)
+	outputPath := filepath.Join(outputDir, fmt.Sprintf("%s-%s", flavor, image))
+	cmd := exec.Command("magick", image, lookupFlavor, "-hald-clut", outputPath)
 	cmd.Dir = currentDirectory
 
-	// Execute the command to generate the output image
 	if err := cmd.Run(); err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
 
-	// Print a message indicating success and the location of the output image
-	fmt.Printf("Successfully generated image with %s flavor in %s/%s-%s\n", flavor, outputDir, flavor, image)
+	fmt.Printf("Successfully generated image with %s flavor in %s\n", flavor, outputPath)
 }
