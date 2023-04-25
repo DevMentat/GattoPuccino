ebf5eaf643edc414353c6bfecff162c4a6538634
DevMentat <devmentat@proton.me>
2023-04-25 10:29:14 +0200

    🧪 test(main_test.go): add multiple test cases to TestMain function

--- main_test.go
+++ main_test.go
@@ -6,27 +6,23 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	// Test case for a successful execution with valid input
-	os.Args = []string{"gattopuccino", "latte", "test-image.jpg"}
-	main()
+	// Save original arguments and restore them after tests
+	origArgs := os.Args
+	defer func() { os.Args = origArgs }()
 
-	// Test case for an invalid flavor
-	os.Args = []string{"gattopuccino", "invalid-flavor", "test-image.jpg"}
-	main()
+	testCases := []struct {
+		args []string
+	}{
+		{[]string{"gattopuccino", "latte", "test-image.jpg"}},
+		{[]string{"gattopuccino", "invalid-flavor", "test-image.jpg"}},
+		{[]string{"gattopuccino"}},
+		{[]string{"gattopuccino", "latte"}},
+		{[]string{"gattopuccino", "test-image.jpg"}},
+		{[]string{"gattopuccino", "latte", "missing-image.jpg"}},
+	}
 
-	// Test case for missing input arguments
-	os.Args = []string{"gattopuccino"}
-	main()
-
-	// Test case for missing image argument
-	os.Args = []string{"gattopuccino", "latte"}
-	main()
-
-	// Test case for missing flavor argument
-	os.Args = []string{"gattopuccino", "test-image.jpg"}
-	main()
-
-	// Test case for a failed execution due to a missing image file
-	os.Args = []string{"gattopuccino", "latte", "missing-image.jpg"}
-	main()
+	for _, tc := range testCases {
+		os.Args = tc.args
+		main()
+	}
 }
